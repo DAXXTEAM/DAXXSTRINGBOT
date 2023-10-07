097898482b3911e8283355455825bc8ca016480c
DAXX TEAM <147141400+DAXXTEAM@users.noreply.github.com>
2023-10-07 21:57:39 +0530

    Update must_join.py

--- DAXXSTRINGBOT/must_join.py
+++ DAXXSTRINGBOT/must_join.py
@@ -24,7 +24,7 @@ async def must_join_channel(bot: Client, msg: Message):
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("</> 𝐎𝐅𝐅𝐈𝐂𝐄", url=link),
+                                InlineKeyboardButton("•─╼⃝𖠁 𝐎𝐅𝐅𝐈𝐂𝐄 𖠁⃝╾─•", url=link),
                             ]
                         ]
                     )
