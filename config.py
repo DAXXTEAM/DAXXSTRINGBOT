f5cc64b0e678f2800a14afd035397cd84838f878
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-07-17 12:53:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "24509589"))
+API_HASH = getenv("API_HASH", "717cf21d94c4934bcbe1eaa1ad86ae75")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-#OWNER_ID = int(getenv("6664582540"))
-OWNER_ID = int(getenv("OWNER_ID", "6664582540"))
-MONGO_DB_URI = getenv("MONGO_DB_URI")
-MUST_JOIN = getenv("MUST_JOIN", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "7493562717:AAH9DGdNRk5g_kCQ6hxLR39szY-WacI8At0")
+OWNER_ID = int(getenv("OWNER_ID", "7427691214"))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://MRDAXX:MRDAXX@mrdaxx.prky3aj.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN = getenv("MUST_JOIN", "gitwizardbypass")
+DAXX_API = getenv("DAXX_API", "daxx-1490?api+key:free")
